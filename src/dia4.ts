3937a2922e50bbfb59b28e1000c493290f57984c
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2024-11-10 20:04:19 -0300

    dia 4!

---
+++ src/dia4.ts
@@ -0,0 +1,11 @@
+function valorTrocado(numero1: number, numero2: number) {
+    var A = numero1;
+    var B = numero2;
+
+    const valorNovo = B;
+    const valorNovo2 = A;
+
+    return console.log(valorNovo, valorNovo2);
+}
+
+console.log(valorTrocado(5, 10));
