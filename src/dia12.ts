95f74fb6e5870b2e74fdac4402ce007a6cfb927d
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2024-11-19 21:02:07 -0300

    dia 12!

--- src/dia12.ts
+++ src/dia12.ts
@@ -2,9 +2,22 @@
 //Faça um algoritmo para converter este tempo em anos, meses e dias. Assuma que cada mês possui sempre 30 dias.
 
 function diasAcidentes(momento: number) {
-    
 
-    
+    const diasPorMes = 30;
+    const mesesPorAno = 12;
 
+    const totalAno = Math.floor(momento / (diasPorMes * mesesPorAno));
 
+    const restanteDiasAno = momento % (diasPorMes * mesesPorAno);
+    const totalMes = Math.floor(restanteDiasAno / diasPorMes);
+
+    const diasRestantes = restanteDiasAno % diasPorMes;
+
+    return {
+        anos: totalAno,
+        meses: totalMes,
+        dias: diasRestantes,
+    };
 }
+
+console.log(diasAcidentes(400));
